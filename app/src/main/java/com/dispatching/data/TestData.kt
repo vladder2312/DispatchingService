d18d0cbc804e6ac6455e7e5e2a8fec2ef1942b6d
Владислав Стяжкин <vladder2312@mail.ru>
2021-02-25 22:03:11 +0300

    Update TestData

--- app/src/main/java/com/dispatching/data/TestData.kt
+++ app/src/main/java/com/dispatching/data/TestData.kt
@@ -1,6 +1,8 @@
 package com.dispatching.data
 
+import com.dispatching.domain.Client
 import com.dispatching.domain.Request
+import com.dispatching.domain.Worker
 import io.reactivex.Observable
 import java.util.*
 
@@ -8,6 +10,21 @@ class TestData {
     val requests = Observable.just(
         Request(
             "123",
+            Client(
+                "1",
+                "Иванов Андрей Сергеевич",
+                "89053441232",
+                "ivanov1324"
+            ),
+            mutableListOf(
+                Worker(
+                    "32",
+                    "Алексеев Антон Владимирович",
+                    "89006553535",
+                    "alskdj75",
+                    Worker.Role.VERIFIER
+                )
+            ),
             Request.Type.CHECKING_COUNTERS,
             Request.Address("Воронеж", "20-летия Октября", 44, null, 12),
             "89512234455",
@@ -18,6 +35,28 @@ class TestData {
         ),
         Request(
             "124",
+            Client(
+                "1",
+                "Иванов Андрей Сергеевич",
+                "89053441232",
+                "ivanov1324"
+            ),
+            mutableListOf(
+                Worker(
+                    "32",
+                    "Левин Максим Олегович",
+                    "89006553535",
+                    "mlevi3434",
+                    Worker.Role.ELECTRICIAN
+                ),
+                Worker(
+                    "33",
+                    "Аксаков Сергей Витальевич",
+                    "89506753434",
+                    "axakser92",
+                    Worker.Role.ELECTRICIAN
+                )
+            ),
             Request.Type.ELECTRIC,
             Request.Address("Воронеж", "Космонавтов", 13, "2", 49),
             "89204123311",
@@ -28,6 +67,21 @@ class TestData {
         ),
         Request(
             "125",
+            Client(
+                "2",
+                "Кузнецов Иван Анатольевич",
+                "89515667483",
+                "qwerty1928"
+            ),
+            mutableListOf(
+                Worker(
+                    "33",
+                    "Ковальский Юрий Михайлович",
+                    "89525633311",
+                    "kovalsky545454",
+                    Worker.Role.WELDER
+                )
+            ),
             Request.Type.WELDING,
             Request.Address("Воронеж", "Пушкина", 76, "у", 219),
             "89991232332",
